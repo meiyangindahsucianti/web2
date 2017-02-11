5a19e83eb48598f60bfb93dd0ef67657e73a9251
wahyudins <wahyudin443@gmail.com>
2017-02-11 18:07:49 +0700

    new coding index dan tambah dan halaman awalnya brooo

--- materi/CRUD/index.php
+++ materi/CRUD/index.php
@@ -1,9 +1,43 @@
-<?php
+<?php 
+
+session_start();
 include 'koneksi.php';
-	if(isset($_GET['delete_id']))
-		$sql_query ="DELETE FROM siswa WHERE id=".$_GET['delete_id'];
-		mysql_query($sql_query);
-		header("location : $_SERVER [PHP_SELF]");
+	if (isset($_POST['login'])) {
+		$username=$_POST['username'];
+		$password= $_POST['password'];
+
+		$sql_query= "select * from user where username ='$username' and password='$password'";
+
+		if (mysql_query($sql_query)) {
+			$num_row=mysql_num_rows(mysql_query($sql_query));
+			if ($num_row ==1) {
+				$SESSION ['username']=$username;
+				?>
+				<script type="text/javascript">
+					alert('anda berhasil login');
+					window.location.href="halaman_awal.php";
+				</script>
+				<?php 
+			}
+			else { ?>
+
+				<script type="text/javascript">
+					alert('maaf bukan anda');
+					window.location.href="index.php"
+				</script>
+				<?php 
+			}
+		}
+	}
+
+	if (isset($_POST['batal'])) {
+			?>
+			<script type="text/javascript">
+				alert('Hatur Nuhun');
+				window.location.href="batal.php";
+			</script>
+			<?php
+	}
 
 ?>
 
@@ -21,18 +55,7 @@ include 'koneksi.php';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-<script type="text/javascript">
-	function edit_id(id){
-		if (confirm('serius ingin di modif bro')){
-			window.location.href ='edit.php?edit_id='+id;
-		}
-	}
-	function delete_id(id){
-		if (confirm('serius ingin di hapus euy')) {
-			window.location.href='index.php?delete_id'+id;
-		}
-	}
-</script>
+
 </head>
 
 <body style="background-color: #eeeeee">
@@ -58,71 +81,36 @@ include 'koneksi.php';
 	</div>
 </div>
 
-<!--  bagian Header menuu   -->
-<div class="container " style="background-color: #000000">
-	<div class="row" style="font-size: 20px">
-		<div class="col-md-12">
-				<ul class="nav navbar-default" style="color: #1a237e">
-					<li class="active" style="background-color: #b9f6ca">
-						<a href="index.php">&nbsp;Beranda&nbsp;</a>
-					</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<li>
-						<a href="tambah.php">&nbsp;menu 1&nbsp;</a>
-					</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<li class="disabled">
-						<a href="#">&nbsp;menu 2&nbsp;</a>
-					</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<li class="disabled">
-						<a href="#">&nbsp;menu 3&nbsp;</a>
-					</li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				</ul>
-		</div>
-	</div>
-</div>
-
 <!--  bagian button   -->
 <div class="container" style="background-color: #b9f6ca">
 	<div class="row" style="height: 486px">
-		<div class="col-md-12">
-			<br><br><br><br>
-			  <h2>Data siswa</h2>        
-			  <table class="table table-bordered" >
-			    <thead>
-			      <tr>
-			        <th><center>No.</center></th>
-			        <th><center>Nama</center></th>
-			        <th><center>NIM</center></th>
-			        <th><center>Alamat</center></th>
-			        <th colspan="2"><center>Aksi</center></th>
-			      </tr>
-			    </thead>
-			    <tbody>
-			    <?php 
-						$sql_query= "SELECT * FROM siswa";
-						$result_set= mysql_query($sql_query);
+		<div class="col-md-4">
+		</div>
 
-						if (mysql_num_rows($result_set)>0) {
-							while ($row=mysql_fetch_row($result_set)) {
-					?>
-			    <tr>
-			    	<td><?php echo $row [0];?></td>
-			    	<td><?php echo $row [1];?></td>
-			    	<td><?php echo $row [2];?></td>
-			    	<td><?php echo $row [3];?></td>
-			    	
-			    	<td><center><a href="javascript:edit_id('<?php echo $row[0];?>')">edit</a></td></center>
-			    	<td><center><a href="javascript:delete_id('<?php echo $row[0];?>')">delete</a></td></center>
-			    </tr>
-			    <?php
-			}
-			}
-			    ?>
-			    </tbody>
-			  </table>
-			  <a href="tambah.php"><button type="button" class="btn btn-info">
-				Tambah Data
-			</button></a>
-			</div>
+		<div class="col-md-4">
+			<br><br><br><br>
+			  <h2>Login</h2>    
+			  <br>
+			  <br>    
+			<form method="post">
+				<div class="form-group form-group-default">
+					<div class="form-group">
+						<label>username</label>
+						<input type="text" style="height: 30px; width: 200px;" class="form-control" name="username" >
+					</div>
+					<div class="form-group">
+						<label>password</label>
+						<input type="password" style="height: 30px; width: 200px;" class="form-control" name="password">
+						<br>
+					<div class="form-group">
+						<button type="submit" style="color: #000000" class="btn btn-info " name="login">Login</button>
+						<button type="submit" style="color: #000000" class="btn btn-danger" name="batal" >Batal</button>
+					</div>
+			</form>
+		</div>
+
+		<div class="col-md-4">
+		</div>
 	</div>
 </div>
 
